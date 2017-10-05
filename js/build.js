93796a1afb0691e407b4020510cc5650bb93cb89
hcarneiro <hcarneiro@weboo.biz>
2017-10-05 14:07:37 +0100

    Fix issue setting App.Storage

--- js/build.js
+++ js/build.js
@@ -117,11 +117,15 @@ Fliplet().then(function() {
                     })
                     .then(function(entry) {
                       return Promise.all([
-                        Fliplet.App.Storage.set('fl-chat-source-id', entry.dataSourceId),
-                        Fliplet.App.Storage.set('fl-chat-auth-email', vmData.email),
-                        Fliplet.App.Storage.set('fl-sms-verification', entry),
-                        Fliplet.Profile.set('email', vmData.email),
-                        Fliplet.Profile.set('phone', entry.data[columns[type + 'To']]),
+                        Fliplet.App.Storage.set({
+                          'fl-chat-source-id': entry.dataSourceId,
+                          'fl-chat-auth-email': vmData.email,
+                          'fl-sms-verification': entry
+                        }),
+                        Fliplet.Profile.set({
+                          'email': vmData.email,
+                          'phone': entry.data[columns[type + 'To']]
+                        }),
                         Fliplet.Hooks.run('onUserVerified', {
                           entry: entry
                         })
